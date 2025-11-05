36cfc11987d076e4200ed330914e1095eccddeea
adela <a@bytebase.com>
2025-11-05 18:56:34 +0100

    fix

--- schema/testschema.sql
+++ schema/testschema.sql
@@ -1,8 +1,4 @@
-CREATE SCHEMA IF NOT EXISTS "testschema";
-
-COMMENT ON SCHEMA "testschema" IS 'test schema for testing purposes';
-
-CREATE TABLE "testschema"."testtable" (
+CREATE TABLE "public"."testtable" (
     "id" serial,
     "name" text NOT NULL,
     "created_at" timestamptz DEFAULT now(),
